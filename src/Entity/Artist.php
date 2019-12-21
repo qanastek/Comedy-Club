c47aa21739b3dc759dcf6c1d675e62d41aa2d59f
qanastek <yanis.labrak@alumni.univ-avignon.fr>
2019-12-21 14:18:55 +0100

    update de la classe artist

--- src/Entity/Artist.php
+++ src/Entity/Artist.php
@@ -9,7 +9,7 @@ use Doctrine\ORM\Mapping as ORM;
 /**
  * @ORM\Entity(repositoryClass="App\Repository\ArtistRepository")
  */
-class Artist extends User
+class Artist
 {
     /**
      * @ORM\Id()
@@ -17,6 +17,21 @@ class Artist extends User
      * @ORM\Column(type="integer")
      */
     private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $image;
+
+    /**
+     * @ORM\Column(type="text", nullable=true)
+     */
+    private $description;
 
     /**
      * @ORM\Column(type="string", length=255, nullable=true)
@@ -109,6 +124,42 @@ class Artist extends User
             $this->events->removeElement($event);
             $event->removeArtist($this);
         }
+
+        return $this;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getImage(): ?string
+    {
+        return $this->image;
+    }
+
+    public function setImage(?string $image): self
+    {
+        $this->image = $image;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(?string $description): self
+    {
+        $this->description = $description;
 
         return $this;
     }
