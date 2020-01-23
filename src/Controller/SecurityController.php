dcbc592a5c8c15024fb1966cf4051a22441c7b2d
qanastek <yanis.labrak@alumni.univ-avignon.fr>
2020-01-23 22:57:57 +0100

    Update

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -14,9 +14,10 @@ class SecurityController extends AbstractController
      */
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
-        // if ($this->getUser()) {
-        //     return $this->redirectToRoute('target_path');
-        // }
+        // If already connected
+        if ($this->getUser()) {
+            return $this->redirectToRoute('admin');
+        }
 
         // get the login error if there is one
         $error = $authenticationUtils->getLastAuthenticationError();
